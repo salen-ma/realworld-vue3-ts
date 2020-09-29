0a902d25ead0f2cef9a17e0c53065404b02a7a2b
mashaoliang <salen.ma@qq.com>
2020-09-29 17:56:19 +0800

    feat: 添加页面模板

---
+++ src/router/index.ts
@@ -0,0 +1,58 @@
+import { RouteRecordRaw, createRouter, createWebHistory  } from 'vue-router'
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: () => import('../views/layout/index'),
+    children: [
+      {
+        path: '',
+        name: 'home',
+        component: () => import(/* webpackChunkName: "home" */ '../views/home/index')
+      },
+      {
+        path: '/login',
+        name: 'login',
+        component: () => import(/* webpackChunkName: "login" */ '../views/login/index'),
+        props: { isLogin: true },
+        meta: { noAuth: true }
+      },
+      {
+        path: '/register',
+        name: 'register',
+        component: () => import(/* webpackChunkName: "register" */ '../views/login/index'),
+        props: { isLogin: false },
+        meta: { noAuth: true }
+      },
+      {
+        path: '/profile/:username',
+        name: 'profile',
+        component: () => import(/* webpackChunkName: "profile" */ '../views/profile/index'),
+      },
+      {
+        path: '/settings',
+        name: 'settings',
+        component: () => import(/* webpackChunkName: "settings" */ '../views/settings/index'),
+        meta: { requiresAuth: true }
+      },
+      {
+        path: '/editor/:slug?',
+        name: 'editor',
+        component: () => import(/* webpackChunkName: "editor" */ '../views/editor/index'),
+        meta: { requiresAuth: true }
+      },
+      {
+        path: '/article/:slug',
+        name: 'article',
+        component: () => import(/* webpackChunkName: "article" */ '../views/article/index'),
+      },
+    ]
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
---
+++ src/views/layout/index.tsx
@@ -0,0 +1,46 @@
+import { defineComponent } from 'vue';
+import { RouterView } from 'vue-router';
+
+export default defineComponent({
+  name: 'Layout',
+  setup() {
+    return () => {
+      <>
+        <nav class="navbar navbar-light">
+          <div class="container">
+            <a class="navbar-brand" href="index.html">conduit</a>
+            <ul class="nav navbar-nav pull-xs-right">
+              <li class="nav-item">
+                <a class="nav-link active" href="">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="">
+                  <i class="ion-compose"></i>&nbsp;New Post
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="">
+                  <i class="ion-gear-a"></i>&nbsp;Settings
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="">Sign up</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <RouterView />
+
+        <footer>
+          <div class="container">
+            <a href="/" class="logo-font">conduit</a>
+            <span class="attribution">
+              An interactive learning project from <a href="https://thinkster.io">Thinkster</a>. Code &amp; design licensed under MIT.
+            </span>
+          </div>
+        </footer>
+      </>
+    }
+  }
+})
